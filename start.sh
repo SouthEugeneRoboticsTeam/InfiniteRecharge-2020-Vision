333b828e016c4e1b10efd08339eddb0b384470d6
Carkoth <kturner44@4j.lane.edu>
2020-02-11 20:21:25 -0800

    update start

--- start.sh
+++ start.sh
@@ -1,21 +1,4 @@
 #!/usr/bin/env bash
 
-wait=15
-
-# Camera paths (note: these paths are static by USB port -- if USB port changes, this path must change)
-cargo_cam="/dev/v4l/by-path/platform-70090000.xusb-usb-0:3.3:1.0-video-index0"
-
-start_camera() {
-  while true; do
-    python3 run.py -n $1 -s $2 -a 60
-  done
-}
-
-echo "$(date) Sleeping $wait seconds..."
-sleep ${wait}
-echo "$(date) Running modprobe..."
-modprobe uvcvideo nodrop=1 timeout=6000
-lsusb
-echo "$(date) Starting vision..."
-
-start_camera cargo ${cargo_cam} &
+cd ~/vision/Infinite-Recharge-2020-Vision/
+python3 run.py
